649ae41e2d32972ea7269c39e2a0baf66d2a3d17
mayalexandera <maya.blevins@gmail.com>
2020-06-07 00:49:46 -0400

    first commit

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,57 @@
 class Startup
+  @@all = []
+  attr_reader :founder, :name, :domain
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain_name, name)
+    self.domain = domain_name
+    self.name = name
+  end
+
+  def sign_contract(startup, venture_capitalist, investment_type, investment_amount)
+    FundingRound.new(self, venture_capitalist, investment_type, investment_amount)
+  end
+
+  def num_funding_rounds
+    FundingRound.all.count{ |fr| fr.startup == self}
+  end
+
+  def total_funds
+    FundingRound.all.inject(0){ |sum, fr| sum + fr.investment if fr.startup == self }
+  end
+
+  def investors
+    vcs = []
+    FundingRound.all.select{|fr| vcs << fr.venture_capitalist if fr.startup == self}
+    vcs.map {|fr| fr.name}.uniq
+  end
+
+  def big_investors
+    vcs = []
+    FundingRound.all.select { |fr| fr.venture_capitalist if fr.startup == self && VentureCapitalist.tres_commas_club.include?(fr.venture_capitalist) } 
+    vcs.map {|fr| fr.name}.uniq
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder_name)
+    @@all.find{ |startup| startup.founder == founder_name}
+  end
+
+  def self.domains
+    @@all.map{ |domain| domain.domain }
+  end
+
+
+  private
+  attr_writer :domain, :name
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,43 @@
 class VentureCapitalist
+@@all = []
+attr_reader :name, :total_worth
+
+def initialize(name, total_worth)
+  @name = name
+  @total_worth = total_worth
+  @@all << self
+end
+
+def offer_contract(startup, type, amount)
+  FundingRound.new(startup, self, type, amount)
+end
+
+def funding_rounds
+  FundingRound.all.count{|fr| fr.venture_capitalist == self }
+end
+
+def portfolio
+  startups = []
+  FundingRound.all.select{|fr| startups << fr.startup if fr.venture_capitalist == self }
+  startups.map{ |startup| startup.name}.uniq
+end
+
+def biggest_investment
+  frs = FundingRound.all.select{|fr| fr.investment if fr.venture_capitalist == self}
+  frs.max{|fr| fr.investment}.venture_capitalist
+end
+
+def invested(domain)
+  frs = FundingRound.all.select{ |fr| fr.venture_capitalist == self && fr.startup.domain == domain}
+  frs.inject(0){ |sum, fr| sum + fr.investment }
+end
+
+def self.all
+  @@all
+end
+
+def self.tres_commas_club
+  @@all.select{ |vc| vc.total_worth > 1000000000}
+end
 
 end
