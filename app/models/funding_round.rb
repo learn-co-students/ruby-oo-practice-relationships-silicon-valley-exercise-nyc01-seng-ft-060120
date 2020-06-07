962c52ebbd33b38524d63c53ac8a69e2d256029e
shadman-a <shadman@outlook.com>
2020-06-07 18:11:46 -0400

    done

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,19 @@
 class FundingRound
+ 
+    attr_reader :venture_capitalists, :startup, :type, :investment
+
+    @@all = []
+
+    def initialize (venture_capitalist, startup, type, investment)
+        @venture_capitalist = venture_capitalist
+        @startup = startup
+        @type = type
+        @investment = investment
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,55 @@
 class VentureCapitalist
 
+    attr_reader :name, :total_worth 
+
+    @@all = []
+
+    def initialize (name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select do |venture_capitalist|
+            venture_capitalist.total_worth >= 1000000000
+        end
+    end
+
+    def offer_contract (startup, type, amount, venture_capitalist)
+        FundingRound.new (startup, type, amount, self)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |round| 
+            round.venture_capitalist == self
+        end
+    end
+    
+    def portfolio
+        funding_rounds.map do |round| 
+            round.startup.domain.uniq
+        end    
+    end
+
+    def bigest_investment
+        funding_rounds.max do |a, b| 
+            a.investment <=> b.investment
+        end
+    end
+
+    def invested(domain)
+        domain_array = funding_rounds.select do |round| 
+            round.startup.domain == domain
+        end
+            total_amt_invested = 0
+        domain_array.each do |funding_round_with_domain|
+            total_amt_invested += funding_round_with_domain.investment
+        end
+        total_amt_invested
+    end
 end
